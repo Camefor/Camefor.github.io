f580726ef7d460718f0e8f7d65db30fb6dece347
wang <2528258531@qq.com>
2019-05-01 20:14:44 +0800

    Site updated: 2019-05-01 20:14:41

---
+++ dist/music.js
@@ -0,0 +1,22 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "Superficial Love",
+        artist: 'Karim Mika / Gabs',
+        url: 'https://music.163.com/song/media/outer/url?id=1316563030.mp3',
+        cover: 'http://p2.music.126.net/tIMbwYpCuScFEdnEb6GTAw==/109951163594454369.jpg?param=130y130',
+        lrc: '@{"briefDesc":"","kalaokLyric":"","kalaokVersion":0,"lyric":"[00:13.02]Blurred lines,missed calls\n[00:16.30]No strings,keep it casual\n[00:19.76]Are we on the same page here?\n[00:22.78]\n[00:25.67]False words,hot,cold\n[00:28.95]Back talk,goning back and forth\n[00:32.59]You say you need distance\n[00:35.68]So why pull me right back in?\n[00:38.54]\n[00:38.99]You're messing with my head tonight\n[00:42.16]And boy I don't give a ****\n[00:45.37]Cos if you play with dynamite\n[00:48.72]One slip,good luck\n[00:51.41]\n[00:51.78]So tell it to me straight,don't lie\n[00:55.21]Oh I've had enough\n[00:58.30]I just got know if it's real\n[01:01.44]Or superfical love?\n[01:04.49]\n[01:14.85]Superfical love\n[01:27.68]Superfical love\n[01:29.45]\n[01:29.63]Two years,where we gonna be?\n[01:32.55]Same life,same friedns,same old melody\n[01:36.59]I don't mean to get ahead of myself but\n[01:40.51]\n[01:40.67]I ain't one to be caught waiting time\n[01:44.25]Life's too short to be spent on your lies\n[01:47.91]You say you needed distance\n[01:51.11]\n[01:52.58]So why pull me right back in?\n[01:55.75]You're messing with my head tonight\n[01:58.98]And boy I don't give a ****\n[02:02.14]Cos if you play with dynamite\n[02:05.49]One slip,good luck\n[02:08.30]\n[02:08.53]So tell it to me straight,don't lie\n[02:11.98]Oh I've had enough\n[02:15.01]I just got know if it's real\n[02:18.11]Or superfical love?\n[02:21.20]\n[02:31.60]Superfical love\n[02:44.33]Superfical love\n[02:46.48]\n[02:47.04]I'm just tryna speak the truth\n[02:51.27]Tell me what do you want\n[02:54.39]From this?\n[02:56.67]\n[02:58.39]Oh,maybe I've misunderstood\n[03:03.88]But tell me where do we go?\n[03:07.56]Where do wo go\n[03:10.31]From here?\n[03:12.23]\n[03:12.75]So tell it to me straight,don't lie\n[03:16.01]Oh I've had enough\n[03:19.10]I just got know if it's real\n[03:22.07]Or superfical love?\n[03:25.13]\n[03:35.31]Superfical love\n[03:41.18]Superfical love\n[03:43.13]\n[03:48.34]Superfical love\n[03:50.42]\n","lyricInfoType":"Lyric_Loaded_Or_Update","lyricUserId":1316563030,"lyricUserName":"是非又欲","lyricUserOffset":-1,"lyricUserTime":0,"lyricVersion":5,"musicId":1316563030,"qfy":false,"transUserId":1316563030,"transUserName":"_NkUryGisH-","transUserTime":0,"translateLyric":"[by:NOTFOUND---404]\n[00:13.02]模糊的关系，漏接的来电\n[00:16.30]无拘无束，随心所欲\n[00:19.76]我们的想法还一致吗\n[00:25.67]虚与委蛇\n[00:28.95]争吵不断\n[00:32.59]你说你需要和我保持一点距离\n[00:35.68]所以为什么又将我挽回\n[00:38.99]今晚你又在耍我\n[00:42.16]但伙计，我才不管呢\n[00:45.37]如果你要来点狠的\n[00:48.72]那就留张纸条 祝你好运\n[00:51.78]所以和我直说吧，别拐弯抹角的\n[00:55.21]我已经受够了\n[00:58.30]我只想知道这是不是真的\n[01:01.44]或只是肤浅的爱\n[01:14.85]肤浅的爱\n[01:27.68]表面的爱而已\n[01:29.63]两年了，我们又身处何处\n[01:32.55]同样的生活节奏和朋友\n[01:36.59]我不想操之过急但是\n[01:40.67]我也不想苦等太久\n[01:44.25]生活短暂容不得你那些把戏\n[01:47.91]你说你需要和我保持一点距离\n[01:52.58]所以为什么又将我挽回\n[01:55.75]你今晚还在耍我\n[01:58.98]但听好了伙计，我不管了\n[02:02.14]如果你要来点真的\n[02:05.49]那就留张纸条 祝你好运\n[02:08.53]所以直说吧，甭想骗我\n[02:11.98]我真的受够了\n[02:15.01]我只想知道这是不是真的\n[02:18.11]或只是虚伪的爱\n[02:31.60]肤浅的爱\n[02:44.33]表面的爱而已\n[02:47.04]我只是试着说出真相而已\n[02:51.27]告诉我你到底在想什么\n[02:54.39]就为这个？\n[02:58.39]哦 或许是我误会了\n[03:03.88]但告诉我我们现在身处何境？\n[03:07.56]身处何境？\n[03:10.31]要去何地？\n[03:12.75]所以就直说吧，甭想骗我\n[03:16.01]我真的已经受够了\n[03:19.10]我只是想知道这是不是真的\n[03:22.07]或只是表面的爱\n[03:35.31]肤浅的\n[03:41.18]爱情\n[03:48.34]肤浅的爱情","translateVersion":4}
+'
+      },
+      {
+        name: 'Erase Me',
+        artist: 'Said The Sky / NÉONHÈART',
+        url: 'https://music.163.com/song/media/outer/url?id=1301861956.mp3',
+        cover: 'http://p1.music.126.net/G105trvU6R2_vqgZWvncuQ==/109951163506191548.jpg',
+      }
+      
+    ]
+});
