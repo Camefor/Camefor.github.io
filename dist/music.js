e869206c595b49cb60517ba0f869a466e1a675a5
wang <2528258531@qq.com>
2019-05-13 15:19:04 +0800

    Site updated: 2019-05-13 15:19:02

--- dist/music.js
+++ dist/music.js
@@ -14,12 +14,6 @@ const ap = new APlayer({
         artist: 'Said The Sky / NÉONHÈART',
         url: 'https://music.163.com/song/media/outer/url?id=1301861956.mp3',
         cover: 'http://p1.music.126.net/G105trvU6R2_vqgZWvncuQ==/109951163506191548.jpg',
-      },
-      {
-        name: 'Paradise',
-        artist: 'Nicky Romero / Deniz Koyu / Walk off the Earth',
-        url: 'https://music.163.com/song/media/outer/url?id=1312104048.mp3',
-        cover: 'http://p2.music.126.net/kaGEo6q88LdkPI_4NODRNA==/109951163564074269.jpg?param=130y130',
       }
       
     ]
